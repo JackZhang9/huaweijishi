92512053bf9cef174bd10412b097ce1757d96477
JackZhang9 <52847412+JackZhang9@users.noreply.github.com>
2022-08-01 16:41:50 +0800

    Add files via upload

---
+++ HJ5 进制转换/snippet (3).cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+ 
+int main() {
+
+    //输入16机制数
+    int strs;
+    cin>>hex>>strs;
+    //dec表示输出10进制的数
+    cout<<dec<<strs<<endl;
+    return 0;
+    
+}
